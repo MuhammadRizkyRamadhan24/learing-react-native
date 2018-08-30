b6aeece4449c3335a4e73153f2fdfb5aac372f68
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2018-08-30 16:33:58 +0700

    first projek

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Judul from './Components/Judul';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Nama  : Muhammad Rizky Ramadhan</Text>
-        <Text>Kelas : XI RPL 4</Text>
-        <Text>Absen : 24</Text>
+        <Judul/>
+        <Text style={warna.warnafont}>Nama  : Muhammad Rizky Ramadhan</Text>
+        <Text style={warna.warnafont}>Kelas : XI RPL 4</Text>
+        <Text style={warna.warnafont}>Absen : 24</Text>
         <Image 
-        style={{height:512, width:384}}
+        style={{height:256, width:192}}
         source={require('./1.jpg')}/>    
       </View>
     );
@@ -19,8 +21,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+const warna = StyleSheet.create({
+  warnafont: {
+    color: '#fff',
   },
 });
